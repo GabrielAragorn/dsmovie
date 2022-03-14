bd085ec029a47c49b8ab28c6b88b175f1d7e3dc2
Gabriel Aragão <gabrielarago4@gmail.com>
2022-03-13 23:04:45 -0300

    First commit with frontend

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,5 +1,12 @@
+import axios from 'axios'
 import MovieCard from 'components/MovieCard'
 import Pagination from 'components/Pagination'
+import { BASE_URL } from 'utils/requests'
+
+//WRONG FORM
+axios.get(`${BASE_URL}/movies`).then(response => {
+  console.log(response.data)
+})
 
 function Listing() {
   return (
