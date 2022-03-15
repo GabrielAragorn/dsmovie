45c8d5d8e2d06604931a012110be1d330de97589
Gabriel Aragão <gabrielarago4@gmail.com>
2022-03-15 12:07:54 -0300

    Props added to React

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -4,12 +4,23 @@ import MovieCard from 'components/MovieCard'
 import Pagination from 'components/Pagination'
 import { BASE_URL } from 'utils/requests'
 import { MoviePage } from 'types/movie'
-import { log } from 'console'
 
 function Listing() {
   //The first hook :)
   //Hook: useState -> Keep the component state
   const [pageNumber, setPageNumber] = useState(0)
+
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true
+  })
 
   /*WRONG FORM
   this way make more than one request
@@ -23,38 +34,29 @@ function Listing() {
       with that way, this request will be execute only when this function is execute too 'cause it has been hooked  
       */
     //doing the request from backend
-    axios.get(`${BASE_URL}/movies`).then(response => {
-      const data = response.data as MoviePage
-      console.log(data)
-
-      setPageNumber(data.number)
-    })
-  }, [])
+    axios
+      //getting the films and sortting then by title (can do with id too)
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+      .then(response => {
+        const data = response.data as MoviePage
+        setPage(data)
+      })
+  }, [pageNumber])
 
   return (
     // that only "<></> guy is called fragment, it does not interfer in nothing :)"
     <>
-      <p>{pageNumber}</p>
-
       <Pagination />
 
       <div className="container">
         <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
+          {/* for each movie in the list "page", the function below will renderize a movie
+          em uma renderização dinâmica de coleção, cada elemento renderizado DEVE possuir um atributo key.*/}
+          {page.content.map(movie => (
+            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard movie={movie} />
+            </div>
+          ))}
         </div>
       </div>
     </>
