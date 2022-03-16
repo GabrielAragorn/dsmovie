a57b4e15b8e96499bdb2c4f216d75c2d2541fa94
Gabriel Aragão <gabrielarago4@gmail.com>
2022-03-16 10:21:11 -0300

    Show score updated

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom'
 function Form() {
   const params = useParams()
 
+  //taking the "movieId" declared in path and giving it to the form card movieId"
   return <FormCard movieId={`${params.movieId}`} />
 }
 
