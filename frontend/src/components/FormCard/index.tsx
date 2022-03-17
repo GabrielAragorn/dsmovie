e0998bf8e9013e789bbedbaa3c9ecfb868d58f9a
Gabriel Aragão <gabrielarago4@gmail.com>
2022-03-17 14:28:19 -0300

    Save the score and e-mail form + useNavigate implemented

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,21 +1,50 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Movie } from 'types/movie'
 import { BASE_URL } from 'utils/requests'
+import { validateEmail } from 'utils/validadeEmail'
 import './styles.css'
 
 type Props = { movieId: string }
 
 function FormCard({ movieId }: Props) {
+  const navigate = useNavigate()
   const [movie, setMovie] = useState<Movie>()
 
   useEffect(() => {
     axios.get(`${BASE_URL}/movies/${movieId}`).then(response => {
       setMovie(response.data)
-      console.log(movieId)
     })
   }, [movieId])
+
+  //interception the form
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    //prevents the loading action after the form submit
+    event.preventDefault()
+    //catching the email and score value from form
+    const email = (event.target as any).email.value
+    const score = (event.target as any).score.value
+
+    if (!validateEmail(email)) {
+      return
+    }
+
+    const config: AxiosRequestConfig = {
+      baseURL: BASE_URL,
+      method: 'PUT',
+      url: '/scores',
+      data: {
+        email: email,
+        movieId: movieId,
+        score: score
+      }
+    }
+
+    axios(config).then(response => {
+      navigate('/')
+    })
+  }
 
   return (
     <div className="dsmovie-form-container">
@@ -26,7 +55,7 @@ function FormCard({ movieId }: Props) {
       />
       <div className="dsmovie-card-bottom-container">
         <h3>{movie?.title}</h3>
-        <form className="dsmovie-form">
+        <form className="dsmovie-form" onSubmit={handleSubmit}>
           <div className="form-group dsmovie-form-group">
             <label htmlFor="email">Informe seu email</label>
             <input type="email" className="form-control" id="email" />
